36211fbc16dd1c4dc0a3a43f8fb9a9c82590bc54
vijaydev133 <vijaydeveloper004@gmail.com>
2024-01-31 09:12:16 +0530

    first commit

--- src/main/java/com/driver/Meeting.java
+++ src/main/java/com/driver/Meeting.java
@@ -1,6 +1,9 @@
 package com.driver;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Meeting {
     private LocalTime startTime;
@@ -10,5 +13,22 @@ public class Meeting {
         this.startTime = startTime;
         this.endTime = endTime;
     }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+   // public void setStartTime(LocalTime startTime) {
+  //      this.startTime = startTime;
+  //  }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    //public void setEndTime(LocalTime endTime) {
+     //   this.endTime = endTime;
+    //}
+
 
 }
